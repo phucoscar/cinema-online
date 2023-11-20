edc4bb473514023d06310e5ca37a82d6ac73f9ae
Vu Kim Phuc <phucchelsea01@gmail.com>
2023-11-21 00:34:06 +0700

    fix bugs check booking seats

--- schedule-sevice/src/main/java/com/example/schedulesevice/dto/response/BookingResponse.java
+++ schedule-sevice/src/main/java/com/example/schedulesevice/dto/response/BookingResponse.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Seat {
-    private Integer row;
-    private Integer column;
+public class BookingResponse {
+    private Integer scheduleId;
+    private List<Seat> seats;
+    private List<Integer> prices;
 }
--- schedule-sevice/src/main/java/com/example/schedulesevice/service/impl/BookingServiceImpl.java
+++ schedule-sevice/src/main/java/com/example/schedulesevice/service/impl/BookingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.schedulesevice.service.impl;
 
 import com.example.schedulesevice.dto.request.BookingDto;
+import com.example.schedulesevice.dto.response.BookingResponse;
 import com.example.schedulesevice.dto.response.Seat;
 import com.example.schedulesevice.entity.Ticket;
 import com.example.schedulesevice.repository.TicketRepository;
@@ -9,6 +10,7 @@ import com.phucvukimcore.base.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +26,26 @@ public class BookingServiceImpl implements BookingService {
             List<Seat> seats = dto.getSeats();
             for (Ticket ticket: ticketsBooked) {
                 for (Seat seat: seats) {
-                    if (seat.getRaw()
+                    if (seat.getRow()
                             .equals(ticket.getSeatNumberHorizontal()) && seat.getColumn()
                             .equals(ticket.getSeatNumberVertical()))
                         return Result.fail("Chỗ ngồi đã được đặt");
                 }
             }
         }
-        return Result.success("Success", dto);
+        BookingResponse response = new BookingResponse();
+        response.setScheduleId(dto.getScheduleId());
+        response.setSeats(dto.getSeats());
+        List<Integer> price = new ArrayList<>();
+        for (Seat seat: dto.getSeats()) {
+            if (seat.getRow() <= 4) {
+                price.add(50000);
+            }
+            else {
+                price.add(80000);
+            }
+        }
+        response.setPrices(price);
+        return Result.success("Success", response);
     }
 }
